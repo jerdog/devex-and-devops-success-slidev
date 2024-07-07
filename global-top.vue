c9fe6feebdcd9a084b15c74ebbabdb85f9a6a2b4
Jeremy Meiss <jeremy.meiss@gmail.com>
2024-07-07 13:44:35 -0500

    initial push

---
+++ global-top.vue
@@ -0,0 +1,6 @@
+<template>
+    <footer class="absolute bottom-0 left-0 right-0 p-2">
+        <img src="/images/twitter-iamjerdog.png" width="15%" style="position:absolute;bottom:10px;left:2%;">
+        <img src="/images/twitter-iamjerdog.png" width="15%" style="position:absolute;bottom:10px;right:2%;">
+    </footer>
+  </template>
